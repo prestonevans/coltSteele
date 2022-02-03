9a93c8c872337caea76c74bd5564bf833dfe6002
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-02 17:58:40 -0700

    fun with css tranform-origin

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
-//document.body.style.backgroundColor = "red";
-//test me
+async function getData() {
+    try {
+        const dataJSON = await fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+        console.log(dataJSON.status)
+        if(!dataJSON.ok) {
+            throw 'ooops';
+        }
+        const data = await dataJSON.json()
+        if (data.error) {
+            throw data.error
+        }
+        console.log(data)
+    } catch(e) {
+        alert(e)
+    }
+}
+try {
+    getData()
+} catch(e) {
+    alert(e)
+}
+// setInterval(getData,2000)
